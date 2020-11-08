2b7007aa191242c14dc478d5565a45f82dc01175
Annie Wormus <annie@wormus.com>
2020-11-08 10:47:45 -0500

    finished parser - before testing

--- parser.c
+++ parser.c
@@ -4,23 +4,34 @@
 
 #include "parser.h"
 
-FILE * inF;        //input: Lexeme list
-FILE * outF;
+//method prototypes
+symbol* parser(lexeme* list);
+void block(symbol* table, int token);
+void constDeclaration(symbol* table, int token);
+void varDeclaration(symbol* table, int token);
+void statement(symbol* table, int token);
+void condition(int token);
+void expression(int token);
+void term(int token);
+void factor(symbol* table, int token);
+int symCheck(int index, symbol * sym, char * token);
+void addToSymTable(symbol* sym, int kind, char* name, int val, int lex, int m, int mark);
+int getNextToken(token_type token);
+int relopCheck(int token, int sym);
 
 int lexIndex = 0, symTableIndex = 0, check;
 token_type token;
-symbol *table;
+symbol *table;      //symbol table
 
 symbol* parser(lexeme* list){
     table = malloc(500 * sizeof(symbol));
     
-    //inF = fopen("lexOUT.txt", "r");
-   // outF = fopen("parserOUT.txt", "w");
-
     int i = 0;
 
     token = lexemeList[lexIndex].tokenVal;
-    block(token);
+    block(table, token);
+
+    //if program doesn't end in a period throw error
     if(token != periodsym){
         printf("ERROR: Period Expected");
     }
@@ -29,17 +40,17 @@ symbol* parser(lexeme* list){
     return table;
 }
 
-void block(int token){
+void block(symbol* table, int token){
     if(token == constsym){
-        constDeclataion();
+        constDeclataion(table, token);
     }else if(token == varsym){
-        varDeclaration();
+        varDeclaration(table, token);
     }else{
-        statement();
+        statement(table, token);
     }
 }
 //variable is declared as a constant
-void constDeclaration(){
+void constDeclaration(symbol* table, int token){
     int i;
     char * varName;
     do{
@@ -56,7 +67,6 @@ void constDeclaration(){
 
         if(check == 0){     //check fails, identifier already exists
             printf("ERROR: Identifier Already Exists");
-            return;
         }else if(check == 1){       //check passes
             strcpy(varName, token);
         }
@@ -65,14 +75,12 @@ void constDeclaration(){
 
         if(token != eqsym){
             printf("ERROR: Identifier Must Be Followed By =");
-            return;
         }
     
         token = getNextToken(token);
 
         if(token != numbersym){
             printf("ERROR: = Must Be Followed By A Number");
-            return;
         }
 
         //if everything checks out, add to symbol table
@@ -83,14 +91,12 @@ void constDeclaration(){
     } while(token == commasym);
     if(token != semicolonsym){
         printf("ERROR: ; Expected");
-        return;
     }
 
     token = getNextToken(token);
 }
 
-
-void varDeclaration(){
+void varDeclaration(symbol* table, int token){
     int numVars;
     if(token == varsym){
         numVars = 0;    //keep track of how many variables you have on the stack
@@ -120,7 +126,7 @@ void varDeclaration(){
     }
 }
 
-void statement(){
+void statement(symbol* table, int token){
     switch(token){
         case identsym:
             //if ident is not in symbol table
@@ -135,32 +141,131 @@ void statement(){
             }
             token = getNextToken(token);
 
-            //EXPRESSION
+            expression(token);
             break;
+
         case beginsym:
+            token = getNextToken(token);
+            statement(table, token);
+            while(token == semicolonsym){
+                token = getNextToken(token);
+                statement(table, token);
+            }
+            if(token != endsym){
+                printf("ERROR: } Expected");
+            }
+            token = getNextToken(token);
             break;
+
         case ifsym:
+            token = getNextToken(token);
+            condition(token);
+            if(token != thensym){
+                printf("ERROR: then Expected");
+            }
+            token = getNextToken(token);
+            statement(table, token);
             break;
+
         case whilesym:
+            token = getNextToken(token);
+            condition(token);
+            if(token != dosym){
+                printf("ERROR: do Expected");
+            }
+            token = getNextToken(token);
+            statement(table, token);
             break;
+
         case readsym:
+            token = getNextToken(token);
+            if(token != identsym){
+                printf("ERROR: Call Must Be Followed By An Identifier");
+            }
+            //check to see if identifier is on symbol table
+            check = symCheck(symTableIndex, table, lexemeList[lexIndex].varname);
+            if(check == 1){
+                printf("ERROR: Undeclared Identifier");
+            }
+            //check if ident is a variable
+
+            token = getNextToken(token);
             break;
+
         case writesym:
+            token = getNextToken(token);
+            if(token != identsym){
+                printf("ERROR: Call Must Be Followed By An Identifier");
+            }
+            check = symCheck(symTableIndex, table, lexemeList[lexIndex].varname);
+            if(check == 1){
+                printf("ERROR: Undeclared Identifier");
+            }
+            token = getNextToken(token);
             break;
     }
 }
 
-void condition(){
+void condition(int token){
+    if(token == oddsym){
+        token = getNextToken(token);
+        expression(token);
+    }else{
+        expression(token);
+        int rcheck = 0;
+        rcheck = relopCheck(token, eqsym);
+        rcheck = relopCheck(token, neqsym);
+        rcheck = relopCheck(token, gtrsym);
+        rcheck = relopCheck(token, geqsym);
+        rcheck = relopCheck(token, lessym);
+        rcheck = relopCheck(token, leqsym);
+        if(rcheck != 1){
+            printf("ERROR: Rel-Op Expected");
+        }
+        token = getNextToken(token);
+        expression(token);
+    }
 
 }
-void expression(){
+void expression(int token){
+    if(token == plussym || token == minussym){
+        token = getNextToken(token);
+    }
+    term(token);
+    while(token == plussym || token == minussym){
+        token = getNextToken(token);
+        term(token);
+    }
 
 }
-void term(){
 
+void term(int token){
+    factor(table, token);
+    while(token == multsym || token == slashsym){
+        token = getNextToken(token);
+        factor(table, token);
+    }
 }
-void factor(){
-    
+
+void factor(symbol* table, int token){
+    if(token == identsym){
+        check = symCheck(symTableIndex, table, lexemeList[lexIndex].varname);
+            if(check == 1){
+                printf("ERROR: Undeclared Identifier");
+            }
+        token = getNextToken(token);
+    }else if(token == numbersym){
+        token = getNextToken(token);
+    }else if(token == lparentsym){
+        token = getNextToken(token);
+        expression(token);
+        if(token != rparentsym){
+            printf("ERROR: ) Expected");
+        }
+        token = getNextToken(token);
+    }else{
+        printf("ERROR: Call Of A Constant Or Variable Is Meaningless");
+    }
 }
 
 //checks symbol table to see if there are conflicting values
@@ -188,5 +293,11 @@ void addToSymTable(symbol* sym, int kind, char* name, int val, int lex, int m, i
 int getNextToken(token_type token){
     lexIndex++;
     return lexemeList[lexIndex].tokenVal;
+}
+
+int relopCheck(int token, int sym){
+    if(token == sym){
+        return 1;
+    }
 }
 
